f51f9c2876ad3b9461ee52922130913506128058
Severin Hasler <severinhasler@hotmail.com>
2021-10-04 19:13:43 +0200

    created Info Section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,55 @@
+import Svg1 from '../../images/svg-1.svg';
+import Svg2 from '../../images/svg-2.svg';
+import Svg3 from '../../images/svg-3.svg';
+import Svg4 from '../../images/svg-4.svg';
+
+export const homeObjOne = {
+    id: 'skills',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Coding',
+    headline: 'Backend to Frontend',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidun ut labore et dolore magna aliquyam',
+    buttonLabel: 'Lorem ipsum',
+    imgStart: true,
+    img: Svg1,
+    alt: 'Guy sitting at Computer',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'education',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Apprenticeship',
+    headline: 'Application engineer',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidun ut labore et dolore magna aliquyam',
+    buttonLabel: 'Lorem ipsum',
+    imgStart: false,
+    img: Svg2,
+    alt: 'Certificate',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'cv',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'CV',
+    headline: 'Who Am I?',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidun ut labore et dolore magna aliquyam',
+    buttonLabel: 'Lorem ipsum',
+    imgStart: true,
+    img: Svg3,
+    alt: 'CV',
+    dark: true,
+    primary: true,
+    darkText: false
+};
